288e94b320750c230fdb24737b3a32521cc526e8
patrykzawadzkisggw <115794809+patrykzawadzkisggw@users.noreply.github.com>
2025-04-19 13:27:05 +0200

    Update drizzle.config.mjs

--- drizzle.config.mjs
+++ drizzle.config.mjs
@@ -1,9 +1,17 @@
-import { defineConfig } from "drizzle-kit";
 import dotenv from "dotenv";
 dotenv.config();
-export default defineConfig({
-  dialect: "postgresql",
+
+/** @type {import('drizzle-kit').Config} */
+export default {
+  schema: './config/db/schema.js',
+  out: './config/drizzle',
+  dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    ssl: process.env.DB_SSL === 'true', 
   },
-});
+};
